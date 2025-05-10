b8dcfd7c345918146ca2df24fc850c840b74345c
Amr Shalaby <53496816+amr1234567@users.noreply.github.com>
2025-05-10 12:35:56 +0300

    Add PatientRepository class with initial methods
    Introduces the `PatientRepository` class implementing the `IPatientRepository` interface.
    Includes methods for retrieving and updating patient information, such as `GetPatientById`,
    `GetPatientWithMedicalHistoryById`, and `UpdatePatientDetails`. All methods currently throw
    `NotImplementedException`, indicating that the functionality is yet to be implemented.
    Utilizes the `FluentResults` library for result handling and supports expressions for related data.

---
+++ HSS.System.V2.DataAccess/Repositories/PatientRepository.cs
@@ -0,0 +1,42 @@
+﻿using FluentResults;
+
+using HSS.System.V2.DataAccess.Contracts;
+using HSS.System.V2.Domain.People;
+
+using System.Linq.Expressions;
+
+namespace HSS.System.V2.DataAccess.Repositories
+{
+    public class PatientRepository : IPatientRepository
+    {
+        public Task<Result<Patient>> GetPatientById(string id, params Expression<Func<Patient, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Patient>> GetPatientWithMedicalHistoryById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Patient>> GetPatientWithMedicalHistoryByNationalId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Patient>> GetPatientByNationalId(string nationalId, params Expression<Func<Patient, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdatePatientDetails(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> CreateMedicalHistoryRecordFromEndedTicket(string ticketId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
