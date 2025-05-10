093e62f89ff1862da6c561bae0daa280279d7f01
mido <loverthe480@gmail.com>
2025-05-10 13:03:06 +0300

    Prescirption Repository

---
+++ HSS.System.V2.DataAccess/Repositories/PatientRepository.cs
@@ -0,0 +1,46 @@
+﻿using FluentResults;
+using HSS.System.V2.DataAccess.Contexts;
+using HSS.System.V2.DataAccess.Contracts;
+using HSS.System.V2.Domain.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSS.System.V2.DataAccess.Repositories
+{
+    public class PatientRepository(AppDbContext context) : IPatientRepository
+    {
+        public Task<Result> CreateMedicalHistoryRecordFromEndedTicket(string ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Patient>> GetPatientById(string id, params Expression<Func<Patient, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Patient>> GetPatientByNationalId(string nationalId, params Expression<Func<Patient, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Patient>> GetPatientWithMedicalHistoryById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Patient>> GetPatientWithMedicalHistoryByNationalId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdatePatientDetails(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HSS.System.V2.DataAccess/Repositories/PrescriptionRepository.cs
@@ -0,0 +1,90 @@
+﻿using FluentResults;
+using HSS.System.V2.DataAccess.Contexts;
+using HSS.System.V2.DataAccess.Contracts;
+using HSS.System.V2.Domain.Prescriptions;
+using HSS.System.V2.Domain.ResultHelpers.Errors;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSS.System.V2.DataAccess.Repositories
+{
+    public class PrescriptionRepository(AppDbContext context) : IPrescriptionRepository
+    {
+        public async Task<Result> CreateMedicalPrescription(Prescription model)
+        {
+            try
+            {
+                if (model is null)
+                    return Result.Fail("Prescription is requred");
+
+                await context.Prescriptions.AddAsync(model);
+                await context.SaveChangesAsync();
+                return Result.Ok();
+            }
+            catch (Exception ex)
+            {
+                return new UnKnownError(ex);
+            }
+        }
+
+        public async Task<Result<Prescription>> GetMedicalPrescriptionById(string id)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                    return Result.Fail("Id is requred");
+
+                var prescriptions = await context.Prescriptions.FindAsync(id);
+                
+                return prescriptions is null ? Result.Fail("Prescription not found") : Result.Ok(prescriptions);
+            }
+            catch (Exception ex)
+            {
+                return new UnKnownError(ex);
+            }
+        }
+
+        public async Task<Result<PrescriptionMedicineItem>> GetMedicalPrescriptionItemById(string id)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                    return Result.Fail("Id is requred");
+
+                var medicines = await context.Prescriptions.Where(item => item.Id == id)
+                    .SelectMany(p => p.Items).FirstOrDefaultAsync();
+
+                return medicines is null ? Result.Fail("there are not Medicines in this Prescription") : Result.Ok(medicines);
+            }
+            catch (Exception ex)
+            {
+                return new UnKnownError(ex);
+            }
+        }
+
+        public async Task<Result> UpdateMedicalPrescription(Prescription model)
+        {
+            try
+            {
+                if (model is null)
+                    return Result.Fail("Prescription is requred");
+
+                var prescription = await context.Prescriptions.FindAsync(model.Id);
+                if (prescription is null)
+                    return Result.Fail("Prescription not found");
+
+                context.Entry(prescription).CurrentValues.SetValues(model);
+                await context.SaveChangesAsync();
+                return Result.Ok();
+            }
+            catch (Exception ex)
+            {
+                return new UnKnownError(ex);
+            }
+        }
+    }
+}
