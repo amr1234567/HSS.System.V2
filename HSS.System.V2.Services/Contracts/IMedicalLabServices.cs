949a8c298f065aefa929fae8ae188a9b3bd89b97
mido <loverthe480@gmail.com>
2025-05-31 18:37:23 +0300

    medicalLab classes

---
+++ HSS.System.V2.Services/Contracts/IMedicalLabServices.cs
@@ -0,0 +1,16 @@
+﻿using FluentResults;
+using HSS.System.V2.Services.DTOs.MedicalLabDTOs;
+using HSS.System.V2.Services.DTOs.PatientDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSS.System.V2.Services.Contracts
+{
+    public interface IMedicalLabServices
+    {
+        Task<Result<MedicaLabTestDTO>> GetTestRequred(string appointmentId);
+    }
+}
